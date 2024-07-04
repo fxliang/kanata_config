62ea2ffd95ef88e459112343dea9828b3cc237fb
fxliang <fx.liang@outlook.com>
2024-07-04 16:28:21 +0800

    initial commit

---
+++ run-kanata.bat
@@ -0,0 +1,5 @@
+@rem run kanata with debug info print
+@rem .\kanata.exe --cfg .\min.kbd -t
+@rem run kanata without debug info print
+@rem .\kanata.exe --cfg .\min.kbd -t
+start "" .\kanata.exe --cfg .\min.kbd
