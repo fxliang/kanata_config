874953d80c0b5ebe6be522e6a83eba1675140eb5
fxliang <fx.liang@outlook.com>
2024-07-19 10:54:02 +0800

    update ci.yml, add log-layer-changes no, add -k param to kill

--- run-kanata-ui.ps1
+++ run-kanata-ui.ps1
@@ -1,1 +1,6 @@
-Start-Process -FilePath ".\kanata-ui.exe" -ArgumentList "--cfg .\min.kbd" -WindowStyle Hidden
+param ( [switch]$k )
+if ($k) {
+  taskkill /im kanata-ui.exe
+} else {
+  Start-Process -FilePath ".\kanata-ui.exe" -ArgumentList "--cfg .\min.kbd" -WindowStyle Hidden
+}
--- run-kanata.ps1
+++ run-kanata.ps1
@@ -1,1 +1,6 @@
-Start-Process -FilePath ".\kanata.exe" -ArgumentList "--cfg .\min.kbd" -WindowStyle Hidden
+param ( [switch]$k )
+if ($k) {
+  taskkill /im kanata.exe
+} else {
+  Start-Process -FilePath ".\kanata.exe" -ArgumentList "--cfg .\min.kbd" -WindowStyle Hidden
+}
